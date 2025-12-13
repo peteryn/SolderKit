458fd50163a7eff5e5fe2b7e6b87a6e3fbcfea6a
Peter Yuan <peter.yuan18@gmail.com>
2025-12-12 20:40:10 -0500

    Add basic Oauth2 flow

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/idtoken"
+)
+
+func main() {
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes:       []string{"email"},
+		RedirectURL:  "http://127.0.0.1:8080/hello",
+		Endpoint:     google.Endpoint,
+	}
+
+	// use PKCE to protect against CSRF attacks
+	// https://www.ietf.org/archive/id/draft-ietf-oauth-security-topics-22.html#name-countermeasures-6
+	verifier := oauth2.GenerateVerifier()
+
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
+	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+
+	// Use the authorization code that is pushed to the redirect
+	// URL. Exchange will do the handshake to retrieve the
+	// initial access token. The HTTP Client returned by
+	// conf.Client will refresh the token as necessary.
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatal(err)
+	}
+	tok, err := conf.Exchange(ctx, code, oauth2.VerifierOption(verifier))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id_token, ok := tok.Extra("id_token").(string)
+	if !ok {
+		log.Fatal("failed to get id_token")
+	}
+	payload, err := idtoken.Validate(ctx, id_token, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(payload.Claims["email"])
+}
